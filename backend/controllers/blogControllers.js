da8b64831ce0dddc7791e00e40e56870cbb9393b
luyce <tanvirabbas64@gmail.com>
2023-09-05 22:32:53 +0500

    Added route and functionality for searching by title

--- backend/controllers/blogControllers.js
+++ backend/controllers/blogControllers.js
@@ -3,13 +3,23 @@ const Blog = require('../models/blogModel')
 const User = require('../models/userModel')
 
 const getBlogs = asyncHandler(async (req, res) => {
-    const blogs = await Blog.find({ user: req.user.id })
+    const blogs = await Blog.find()
     res.status(200).json(blogs)
 })
 
 const getBlog = asyncHandler(async (req, res) => {
-    const blog = await Blog.findById(req.params.id, req.body, { user: req.user.id })
+    const blog = await Blog.findById(req.params.id, req.body)
     res.status(200).json({ id: blog.id, title: blog.title, subTitle: blog.subTitle, description: blog.description, tags: blog.tags })
+})
+
+const searchBlog = asyncHandler(async (req, res) => {
+    const { query } = req.query
+    try {
+        const blogs = await Blog.find({ title: { $regex: new RegExp(query, 'i') } });
+        res.json({ blogs });
+    } catch (err) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
 })
 
 const setBlog = asyncHandler(async (req, res) => {
@@ -22,17 +32,11 @@ const setBlog = asyncHandler(async (req, res) => {
 })
 
 const updateBlog = asyncHandler(async (req, res) => {
-    const blog = await Blog.findById(req.params.id)
     if (!blog) {
         res.status(400)
         throw new Error('Blog Not Found')
     }
-    const user = await User.findById(req.user.id)
-    if (!user) {
-        res.status(401)
-        throw new Error('User Not Found')
-    }
-    if (blog.user.toString() !== user.id) {
+    if (blog.user.toString() !== req.user.id) {
         res.status(401)
         throw new Error('User Not Authorized')
     }
@@ -46,12 +50,11 @@ const deleteBlog = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('Blog Not Found')
     }
-    const user = await User.findById(req.user.id)
-    if (!user) {
+    if (!req.user) {
         res.status(401)
         throw new Error('User Not Found')
     }
-    if (blog.user.toString() !== user.id) {
+    if (blog.user.toString() !== req.user.id) {
         res.status(401)
         throw new Error('User Not Authorized')
     }
@@ -59,4 +62,4 @@ const deleteBlog = asyncHandler(async (req, res) => {
     res.status(200).json(req.params.id)
 })
 
-module.exports = { getBlogs, getBlog, setBlog, updateBlog, deleteBlog }
+module.exports = { getBlogs, getBlog,searchBlog ,setBlog, updateBlog, deleteBlog }
--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -1,8 +1,9 @@
-const { getBlogs, getBlog, setBlog, updateBlog, deleteBlog } = require('../controllers/blogControllers')
+const { getBlogs, getBlog,searchBlog ,setBlog, updateBlog, deleteBlog } = require('../controllers/blogControllers')
 const express = require('express')
 const router = express.Router()
 const protect = require('../middleware/authMiddleware')
 
+router.get('/search', searchBlog)
 router.route('/').get(getBlogs).post(protect, setBlog)
 router.route('/:id').put(protect, updateBlog).delete(protect, deleteBlog).get(getBlog)
 
